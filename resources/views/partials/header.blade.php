4f3770f6cc66511791fbd3715f7e6a21e6bcc80d
andreafontaneto <andrea.fontaneto@gmail.com>
2022-01-24 20:55:28 +0100

    add logo + favicon

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,11 @@
 <header class="mb-5 af-header">
 
-  <div class="container">
+  <div class="container d-flex align-items-center">
+    
+    <div class="mr-3" style="width: 50px">
+      <img src="{{ asset('img/kisspng-photographic-film-vector-graphics-computer-icons-p-filmroll-icon-myiconfinder-5c579dd0458661.7829945215492459042848.png') }}" alt="">
+    </div>
+    
     <ul class="nav nav-pills">
       <li class="nav-item">
         <a class="nav-link {{ (Route::currentRouteName() === 'home') ? 'af-active' : '' }}" aria-current="page" href="{{ route('home') }}">HOME</a>
